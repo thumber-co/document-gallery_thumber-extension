9bcfb6d1637b3acc77bdcf61edccccd2315b063c
thumber-co <support@thumber.co>
2015-07-11 19:05:33 -0500

    initial commit

---
+++ document-gallery-thumber-extension.php
@@ -0,0 +1,173 @@
+<?php
+defined('WPINC') OR exit;
+
+/*
+ Plugin Name: Document Gallery - Thumber Extension
+Plugin URI: http://wordpress.org/extend/plugins/document-gallery/
+Description: An extension for the Document Gallery plugin to support Thumber.co API.
+Version: 1.0
+Author: Dan Rossiter
+Author URI: http://danrossiter.org/
+*/
+
+add_action('dg_thumbers', array('DocumentGalleryThumberExtension', 'thumbersFilter'));
+add_filter('allowed_http_origin', array('DocumentGalleryThumberExtension', 'allowThumberWebhooks'), 10, 2);
+add_action('admin_post_nopriv_' . DocumentGalleryThumberExtension::ThumberAction, array('ThumberClient', 'receiveResponse'), 5, 0);
+
+add_filter('upload_mimes', array('DocumentGalleryThumberExtension', 'customUploadMimeTypes'));
+
+    DocumentGalleryThumberExtension::init();
+class DocumentGalleryThumberExtension {
+
+   const ThumberAction = 'dg_thumber_extension';
+
+   /**
+    * @var string Path to the base directory for this extension.
+    */
+   private static $path;
+   
+   private static $webhook;
+   
+   private static $nonce_separator = '_';
+    
+   /**
+    * Initializes the static values for this class.
+    */
+   public static function init() {
+      self::$path = plugin_dir_path(__FILE__);
+      self::$webhook = admin_url('admin-post.php?action=' . self::ThumberAction);
+      
+      include_once self::$path . 'thumber-client/client.php';
+      
+      ThumberClient::setCallback(array(__CLASS__, 'webhookCallback'));
+      ThumberClient::setUid('ecf110e1-d64d-47b0-8c7c-54c851d4e8ea');
+      ThumberClient::setUserSecret('_lWFVAlNnNEkNn83jbFKcVZ_DVKQN1ha4g9RHnvsbAM');
+   }
+
+   /**
+    * @param $mimes array
+    * @return array
+    */
+   public static function customUploadMimeTypes($mimes) {
+      $mimes['pub'] = 'application/x-mspublisher';
+      return $mimes;
+   }
+
+   /**
+    * @param $resp ThumberResp
+    */
+   public static function webhookCallback($resp) {
+      include_once self::$path . 'thumber-client/response.php';
+
+      DG_Logger::writeLog(DG_LogLevel::Detail, 'Entering webhook callback.');
+      
+      $nonce = $resp->getNonce();
+      $split = explode(self::$nonce_separator, $nonce);
+      if ($resp->getSuccess() && count($split) == 2) {
+         $ID = absint($split[0]);
+         
+         $tmpfile = get_temp_dir() . self::getTempFile(get_temp_dir());
+         file_put_contents($tmpfile, $resp->getDecodedData());
+         
+         DG_Thumber::setThumbnail($ID, $tmpfile, array(__CLASS__, 'getThumberThumbnail'));
+      } elseif (self::logEnabled()) {
+         $ID = (count($split) > 0) ? $split[0] : $nonce;
+         DG_Logger::writeLog(DG_LogLevel::Warning, "Thumber failed for $ID: " . $resp->getError());
+      }
+   }
+   
+   public static function allowThumberWebhooks($origin, $origin_arg) {
+      return $origin || (isset($_REQUEST['action']) && $_REQUEST['action'] === DG_THUMBER_ACTION);
+   }
+    
+   /**
+    *
+    * @param multitype:callable $thumbers The thumbnail generators being used by Document Gallery.
+    * @return multitype:callable The thumbnail generators being used by Document Gallery, with the
+    * default Google Drive generaor replaced if present.
+    */
+   public static function thumbersFilter($thumbers) {
+      if (self::logEnabled()) {
+         DG_Logger::writeLog(DG_LogLevel::Detail, 'Adding ' . __CLASS__ . '::getThumberThumbnail()');
+      }
+
+      // avoid values being removed as a result of current user but also include any MIME types
+      // that are added outside of the default WP values
+      $wp_types = array_merge(wp_get_mime_types(), get_allowed_mime_types());
+
+      // TODO: Needs to be cached
+      $allowed = array_intersect($wp_types, ThumberClient::getMimeTypes());
+      $allowed = array_keys($allowed);
+
+      $thumbers[implode('|', $allowed)] = array(__CLASS__, 'getThumberThumbnail');
+      return $thumbers;
+   }
+   
+   public static function getThumberThumbnail($ID, $pg = 1) {
+      if (self::logEnabled()) {
+         DG_Logger::writeLog(DG_LogLevel::Detail, "Getting thumbnail for $ID.");
+      }
+      
+      include_once self::$path . 'thumber-client/client.php';
+      include_once self::$path . 'thumber-client/request.php';
+       
+      $url = wp_get_attachment_url($ID);
+      $mime_type = get_post_mime_type($ID);
+      
+      if (!$url || !$mime_type) {
+         return false;
+      }
+      
+      $options = DocumentGallery::getOptions();
+      $geometry = "{$options['thumber']['width']}x{$options['thumber']['height']}";
+      
+      $req = new ThumberReq();
+      $req->setCallback(self::$webhook);
+      $req->setMimeType($mime_type);
+      $req->setNonce($ID. self::$nonce_separator . md5(microtime()));
+      $req->setPg($pg);
+      $req->setUrl($url);
+      $req->setGeometry($geometry);
+      
+      $resp = ThumberClient::sendRequest($req);
+
+      if (is_wp_error($resp)) {
+         DG_Logger::writeLog(DG_LogLevel::Error, 'Failed to post: ' . $resp->get_error_message());
+      } else {
+         DG_Logger::writeLog(DG_LogLevel::Detail, 'Response: ' . $resp['body']);
+      }
+      
+      return false;
+   }
+    
+   /**
+    * @param string $dir The directory that the generated filename is destined.
+    * @param string $ext The file extension to generate the file with.
+    * @return string The filename to be used relative to the directory provided.
+    */
+   private static function getTempFile($dir, $ext = 'png') {
+      static $base;
+
+      if (!isset($base)) {
+         $base = md5(time());
+      }
+
+      return wp_unique_filename(untrailingslashit($dir), "$base.$ext");
+   }
+
+   /**
+    * @return bool Whether debug logging is currently enabled.
+    */
+   public static function logEnabled() {
+      return class_exists('DG_Logger') && DG_Logger::logEnabled();
+   }
+
+   /**
+    * Blocks instantiation. All functions are static.
+    */
+   private function __construct() {
+
+   }
+}
+
+?>
---
+++ thumber-client/client.php
@@ -0,0 +1,209 @@
+<?php
+defined('WPINC') OR exit;
+
+/**
+ * Hanlde processing POST callbacks (which will include the generated thumbnail or an error msg).
+ */
+if (isset($_GET['response'])) {
+   ThumberClient::receiveResponse();
+}
+
+/**
+ * Class to process sending requests and receiving responses.
+ */
+ThumberClient::init();
+class ThumberClient
+{
+   private static $thumberUserAgent;
+
+   private static $thumberClientPath;
+
+   const ThumberServerHost = 'api.lvh.me:3000';
+
+   const ThumberServerCreatePath = '/create.json';
+
+   const ThumberServerMimeTypesPath = '/mime_types.json';
+
+   /**
+    * Initializes ThumberClient.
+    */
+   public static function init() {
+      if (!isset(self::$thumberUserAgent)) {
+         self::$thumberUserAgent = 'Thumber Client 1.0 (PHP ' . phpversion() . '; ' . php_uname() . ')';
+         self::$thumberClientPath = dirname(__FILE__) . '/';
+      }
+   }
+
+   /**
+    * @var string UID for the user accessing the Thumber API.
+    */
+   private static $uid;
+
+   public static function setUid($uid) {
+      self::$uid = $uid;
+   }
+
+   /**
+    * @var string The user secret assoicataed with the UID for the user
+    * accessing the Thumber API.
+    */
+   private static $userSecret;
+
+   public static function setUserSecret($userSecret) {
+      self::$userSecret = $userSecret;
+   }
+
+   /**
+    * @var string The URL pointing to this file's directory. Must be publically
+    * accessible for Thumebr API to send generated thumbnail following request.
+    */
+   private static $webhook;
+
+   private static function setWebhook($webhook) {
+      self::$webhook = $webhook;
+   }
+
+   /**
+    * @var callable The method to be invoked when response arrives.
+    */
+   private static $callback;
+
+   public static function setCallback($callback) {
+      self::$callback = $callback;
+   }
+
+   /**
+    * Sends the provided request to the API endpoint.
+    *
+    * @param ThumberReq $req The request to be sent. UID, callback, and timestamp
+    * will be written by client. Additionally, nonce will be set if not already set.
+    * @return array containing data about success of the cURL request.
+    */
+   public static function sendRequest($req) {
+      include_once self::$thumberClientPath . 'request.php';
+
+      if (!($req instanceof ThumberReq)) {
+         die('Request must be of type ThumberReq.');
+      }
+
+      $req->setTimestamp(time());
+
+      $uid = $req->getUid();
+      if (empty($uid)) {
+         $req->setUid(self::$uid);
+      }
+
+      $callback = $req->getCallback();
+      if (empty($callback)) {
+         $req->setCallback(self::$directoryUrl . 'client.php?response=1');
+      }
+
+      $nonce = $req->getNonce();
+      if (empty($nonce)) {
+         $req->setNonce();
+      }
+
+      $req->setChecksum($req->computeChecksum(self::$userSecret));
+
+      if (!$req->isValid(self::$userSecret)) {
+         if (DocumentGalleryThumberExtension::logEnabled()) {
+            DG_Logger::writeLog(DG_LogLevel::Detail, 'Request is invalid. Not sending.');
+         }
+
+         return new WP_Error();
+      }
+
+      $json = $req->toJson();
+      if (DocumentGalleryThumberExtension::logEnabled()) {
+         DG_Logger::writeLog(DG_LogLevel::Detail, "Sending request: $json");
+      }
+
+      $args = array(
+          'headers'      => array('Content-Type' => 'application/json'),
+          'user-agent'   => self::$thumberUserAgent,
+          'body'         => $json
+      );
+
+      // caller should handle failures sensibly
+      return wp_remote_post('http://' . self::ThumberServerHost . self::ThumberServerCreatePath, $args);
+   }
+
+   /**
+    * Processes the POST request, generating a ThumberResponse, validating, and passing the result to $callback.
+    */
+   public static function receiveResponse() {
+      include_once self::$thumberClientPath . 'response.php';
+
+      if (!isset(self::$callback) && DocumentGalleryThumberExtension::logEnabled()) {
+         DG_Logger::writeLog(DG_LogLevel::Error, __CLASS__ . '::$callback must be initialized.');
+      }
+
+      $json = stream_get_contents(fopen('php://input', 'r'));
+      $resp = ThumberResp::parseJson($json);
+
+      if (is_null($resp)) {
+         if (DocumentGalleryThumberExtension::logEnabled()) {
+            DG_Logger::writeLog(DG_LogLevel::Error, 'Failed to parse JSON in POST body: ' . $json);
+         }
+      } elseif (!$resp->isValid(self::$userSecret) && DocumentGalleryThumberExtension::logEnabled()) {
+         DG_Logger::writeLog(DG_LogLevel::Error, 'Received invalid response: ' . $json);
+      }
+
+      // response passed validation -- relay to callback function
+      call_user_func(self::$callback, $resp);
+   }
+
+   /**
+    * @return array The supported MIME types reported by the Thumber server.
+    */
+   public static function getMimeTypes() {
+      $args = array(
+          'headers'      => array('Content-Type' => 'application/json'),
+          'user-agent'   => self::$thumberUserAgent
+      );
+
+      // caller should handle failures sensibly
+      $resp = wp_remote_get('http://' . self::ThumberServerHost . self::ThumberServerMimeTypesPath, $args);
+
+      $ret = is_array($resp) ? json_decode($resp['body'], true) : array();
+
+      if (DocumentGalleryThumberExtension::logEnabled()) {
+         DG_Logger::writeLog(DG_LogLevel::Detail, 'Thumber MIME Types: ' . implode(', ', $ret));
+      }
+
+      return $ret;
+   }
+
+   /**
+    * Sends cURL request to Thumber server.
+    * @param $ch resource The curl connection to be used.
+    * @return array The result of the request.
+    */
+   private static function sendToThumber($ch) {
+      // execute post, storing useful information about result
+      $response = curl_exec($ch);
+      $error = curl_error($ch);
+      $result = array (
+          'header'          => '',
+          'body'            => '',
+          'curl_error'      => '',
+          'http_code'       => '',
+          'last_url'        => ''
+      );
+
+      if ($error === '') {
+         $header_size = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
+
+         $result['header']    = substr($response, 0, $header_size);
+         $result['body']      = substr($response, $header_size);
+         $result['http_code'] = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+         $result['last_url']  = curl_getinfo($ch, CURLINFO_EFFECTIVE_URL);
+      } else {
+         $result ['curl_error'] = $error;
+      }
+
+      curl_close($ch);
+
+      return $result;
+   }
+}
