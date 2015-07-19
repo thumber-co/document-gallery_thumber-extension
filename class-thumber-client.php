3ba99ffc65af0a92e2dd25573ed08bc342c5d8b5
thumber-co <support@thumber.co>
2015-07-19 00:00:39 -0500

    Now using https://github.com/thumber-co/php-client/ unmodified and simply extending the client to add WP-specific functionality.

---
+++ class-thumber-client.php
@@ -0,0 +1,143 @@
+<?php
+defined('WPINC') OR exit;
+
+DG_ThumberClient::init();
+
+class DG_ThumberClient extends ThumberClient {
+
+	/**
+	 * @var DG_ThumberClient Backs the getter.
+	 */
+	private static $instance;
+
+	/**
+	 * @return DG_ThumberClient The singleton instance.
+	 */
+	public static function getInstance() {
+		return isset(self::$instance) ? self::$instance : (self::$instance = new DG_ThumberClient());
+	}
+
+	/**
+	 * Enforce singleton.
+	 */
+	private function __construct() {
+	}
+
+	/**
+	 * Initialized class members.
+	 */
+	public static function init() {
+		self::$uid = 'TODO';
+		self::$userSecret = 'TODO';
+		self::$thumberUserAgent = 'Document Gallery Thumber Client 1.0 (PHP ' . phpversion() . '; ' . php_uname() . ')';
+	}
+
+	/**
+	 * Sends HTTP request to Thumber server.
+	 * @param $type string GET or POST
+	 * @param $url string The URL endpoint being targeted.
+	 * @param $httpHeaders array The headers to be sent.
+	 * @param $body string The POST body. Ignored if type is GET.
+	 * @return array The result of the request.
+	 */
+	protected function sendToThumber($type, $url, $httpHeaders, $body = '') {
+		$headers = array();
+		foreach ($httpHeaders as $v) {
+			$kvp = explode(':', $v);
+			$headers[trim($kvp[0])] = trim($kvp[1]);
+		}
+
+		$result = array (
+			'header'          => '',
+			'body'            => '',
+			'http_code'       => '',
+			'last_url'        => ''
+		);
+		$args = array(
+			'headers'      => $headers,
+			'user-agent'   => self::$thumberUserAgent
+		);
+
+		switch ($type) {
+			case 'GET':
+				$resp = wp_remote_get($url, $args);
+				break;
+
+			case 'POST':
+				if (!empty($body)) {
+					$args['body'] = $body;
+				}
+
+				$resp = wp_remote_post($url, $args);
+				break;
+
+			default:
+				$err = 'Invalid HTTP type given: ' . $type;
+				self::handleError($err);
+				$result['error'] = 'Invalid HTTP type given: ' . $type;
+		}
+
+		if (isset($resp)) {
+			if (!is_wp_error($resp)) {
+				$result['body'] = $resp['body'];
+				$result['http_code'] = $resp['response']['code'];
+			} else {
+				$result['error'] = $resp->get_error_message();
+				$result['http_code'] = $resp->get_error_code();
+			}
+		}
+
+		return $result;
+	}
+
+	/**
+	 * Processes the POST request, generating a ThumberResponse, validating, and passing the result to $callback.
+	 * If not using client.php as the webhook, whoever receives webhook response should first invoke this method to
+	 * validate response.
+	 */
+	public function receiveResponse() {
+		$resp = parent::receiveResponse();
+
+
+		$nonce = $resp->getNonce();
+		$split = explode(DocumentGalleryThumberExtension::NonceSeparator, $nonce);
+		if ($resp->getSuccess() && count($split) == 2) {
+			$ID = absint($split[0]);
+			$tmpfile = get_temp_dir() . self::getTempFile(get_temp_dir());
+
+			file_put_contents($tmpfile, $resp->getDecodedData());
+
+			DG_Thumber::setThumbnail($ID, $tmpfile, array(__CLASS__, 'getThumberThumbnail'));
+			if (DocumentGalleryThumberExtension::logEnabled()) {
+				DG_Logger::writeLog( DG_LogLevel::Detail, "Received thumbnail from Thumber for attachment #{$split[0]}." );
+			}
+		} elseif (DocumentGalleryThumberExtension::logEnabled()) {
+			$ID = (count($split) > 0) ? $split[0] : $nonce;
+			DG_Logger::writeLog(DG_LogLevel::Warning, "Thumber was unable to process attachment #$ID: " . $resp->getError());
+		}
+	}
+
+	/**
+	 * @param $err string Fires on fatal error.
+	 */
+	protected function handleError($err) {
+		if (DocumentGalleryThumberExtension::logEnabled()) {
+			DG_Logger::writeLog(DG_LogLevel::Error, $err);
+		}
+	}
+
+	/**
+	 * @param string $dir The directory that the generated filename is destined.
+	 * @param string $ext The file extension to generate the file with.
+	 * @return string The filename to be used relative to the directory provided.
+	 */
+	private static function getTempFile($dir, $ext = 'png') {
+		static $base;
+
+		if (!isset($base)) {
+			$base = md5(time());
+		}
+
+		return wp_unique_filename(untrailingslashit($dir), "$base.$ext");
+	}
+}
--- document-gallery-thumber-extension.php
+++ document-gallery-thumber-extension.php
@@ -10,75 +10,74 @@ defined('WPINC') OR exit;
  Author URI: http://danrossiter.org/
 */
 
+include_once plugin_dir_path(__FILE__) . 'thumber-client/client.php';
+include_once plugin_dir_path(__FILE__) . 'class-thumber-client.php';
+
 add_action('dg_thumbers', array('DocumentGalleryThumberExtension', 'thumbersFilter'));
 add_filter('allowed_http_origin', array('DocumentGalleryThumberExtension', 'allowThumberWebhooks'), 10, 2);
-add_action('admin_post_nopriv_' . DocumentGalleryThumberExtension::ThumberAction, array('ThumberClient', 'receiveResponse'), 5, 0);
-add_filter('upload_mimes', array('DocumentGalleryThumberExtension', 'customUploadMimeTypes'));
+add_filter('upload_mimes', array('DocumentGalleryThumberExtension', 'customMimeTypes'));
+add_action('admin_post_nopriv_' . DocumentGalleryThumberExtension::ThumberAction, array(DG_ThumberClient::getInstance(), 'receiveResponse'), 5, 0);
 
 DocumentGalleryThumberExtension::init();
 class DocumentGalleryThumberExtension {
 
+   /**
+    * @const string Name of the action performed in the webhook.
+    */
    const ThumberAction = 'dg_thumber_extension';
+
+   /**
+    * @const string Used in nonce to separate the attachment ID from the "random" segment.
+    */
+   const NonceSeparator = '_';
 
    /**
     * @var string Path to the base directory for this extension.
     */
    private static $path;
-   
+
+   /**
+    * @var string URL to webhook.
+    */
    private static $webhook;
-   
-   private static $nonce_separator = '_';
-    
+
+   /**
+    * @var DG_ThumberClient The thumber client isntance.
+    */
+   private static $client;
+
    /**
     * Initializes the static values for this class.
     */
    public static function init() {
-      self::$path = plugin_dir_path(__FILE__);
-      self::$webhook = admin_url('admin-post.php?action=' . self::ThumberAction);
-      
-      include_once self::$path . 'thumber-client/client.php';
-      
-      ThumberClient::setCallback(array(__CLASS__, 'webhookCallback'));
-      ThumberClient::setUid('TODO');
-      ThumberClient::setUserSecret('TODO');
+      if (!isset(self::$path)) {
+         self::$path = plugin_dir_path(__FILE__);
+         self::$webhook = admin_url('admin-post.php?action=' . self::ThumberAction);
+         self::$client = DG_ThumberClient::getInstance();
+      }
    }
 
    /**
-    * @param $mimes array
-    * @return array
+    * TODO: This should be a configurable option and should include all Thumber types not default WP-supported.
+    * @param $mimes array The MIME types WP knows about.
+    * @return array Modified MIME types -- adding additional supported types.
     */
-   public static function customUploadMimeTypes($mimes) {
-      $mimes['pub'] = 'application/x-mspublisher';
+   public static function customMimeTypes($mimes) {
+      $mimes['pub'] = 'application/mspublisher';
       return $mimes;
    }
 
    /**
-    * @param $resp ThumberResp
+    * WP by default will not handle POSTs from Thumber so add a special case for the action we want to handle.
+    * @param $origin
+    * @param $origin_arg
+    *
+    * @return bool Whether WP will handle the action.
     */
-   public static function webhookCallback($resp) {
-      include_once self::$path . 'thumber-client/response.php';
-      
-      $nonce = $resp->getNonce();
-      $split = explode(self::$nonce_separator, $nonce);
-      if ($resp->getSuccess() && count($split) == 2) {
-         $ID = absint($split[0]);
-         
-         $tmpfile = get_temp_dir() . self::getTempFile(get_temp_dir());
-         file_put_contents($tmpfile, $resp->getDecodedData());
-         
-         DG_Thumber::setThumbnail($ID, $tmpfile, array(__CLASS__, 'getThumberThumbnail'));
-
-         DG_Logger::writeLog(DG_LogLevel::Detail, "Received thumbnail from Thumber for attachment #{$split[0]}.");
-      } elseif (self::logEnabled()) {
-         $ID = (count($split) > 0) ? $split[0] : $nonce;
-         DG_Logger::writeLog(DG_LogLevel::Warning, "Thumber was unable to process attachment #$ID: " . $resp->getError());
-      }
-   }
-   
    public static function allowThumberWebhooks($origin, $origin_arg) {
       return $origin || (isset($_REQUEST['action']) && $_REQUEST['action'] === self::ThumberAction);
    }
-    
+
    /**
     *
     * @param multitype:callable $thumbers The thumbnail generators being used by Document Gallery.
@@ -95,63 +94,57 @@ class DocumentGalleryThumberExtension {
       $wp_types = array_merge(wp_get_mime_types(), get_allowed_mime_types());
 
       // TODO: Needs to be cached
-      $allowed = array_intersect($wp_types, ThumberClient::getMimeTypes());
+      $allowed = array_intersect($wp_types, self::$client->getMimeTypes());
       $allowed = array_keys($allowed);
 
       $thumbers[implode('|', $allowed)] = array(__CLASS__, 'getThumberThumbnail');
       return $thumbers;
    }
-   
+
+   /**
+    * @param int $ID The attachment ID.
+    * @param int $pg The page to thumbnail.
+    *
+    * @return bool Always false. Asynchronously set the thumbnail in webhook later.
+    */
    public static function getThumberThumbnail($ID, $pg = 1) {
       if (self::logEnabled()) {
          DG_Logger::writeLog(DG_LogLevel::Detail, "Getting thumbnail for $ID.");
       }
-      
+
       include_once self::$path . 'thumber-client/client.php';
       include_once self::$path . 'thumber-client/request.php';
-       
+
       $url = wp_get_attachment_url($ID);
       $mime_type = get_post_mime_type($ID);
-      
+
       if (!$url || !$mime_type) {
          return false;
       }
-      
+
       $options = DocumentGallery::getOptions();
       $geometry = "{$options['thumber']['width']}x{$options['thumber']['height']}";
-      
+
       $req = new ThumberReq();
       $req->setCallback(self::$webhook);
       $req->setMimeType($mime_type);
-      $req->setNonce($ID. self::$nonce_separator . md5(microtime()));
+      $req->setNonce($ID. self::NonceSeparator . md5(microtime()));
       $req->setPg($pg);
       $req->setUrl($url);
       $req->setGeometry($geometry);
-      
-      $resp = ThumberClient::sendRequest($req);
 
-      if (is_wp_error($resp)) {
-         DG_Logger::writeLog(DG_LogLevel::Error, 'Failed to post: ' . $resp->get_error_message());
-      } else {
-         DG_Logger::writeLog(DG_LogLevel::Detail, 'Response: ' . $resp['body']);
+      $resp = self::$client->sendRequest($req);
+
+      if (self::logEnabled())
+      {
+         if (is_wp_error($resp)) {
+            DG_Logger::writeLog(DG_LogLevel::Error, 'Failed to post: ' . $resp->get_error_message());
+         } else {
+            DG_Logger::writeLog(DG_LogLevel::Detail, 'Response: ' . $resp['body']);
+         }
       }
-      
+
       return false;
-   }
-    
-   /**
-    * @param string $dir The directory that the generated filename is destined.
-    * @param string $ext The file extension to generate the file with.
-    * @return string The filename to be used relative to the directory provided.
-    */
-   private static function getTempFile($dir, $ext = 'png') {
-      static $base;
-
-      if (!isset($base)) {
-         $base = md5(time());
-      }
-
-      return wp_unique_filename(untrailingslashit($dir), "$base.$ext");
    }
 
    /**
@@ -168,5 +161,3 @@ class DocumentGalleryThumberExtension {
 
    }
 }
-
-?>
